2b18461964381116b4700af8411beb28e06b1625
Norman Breau <norman@normanbreau.com>
2020-04-25 21:42:41 -0300

    tests

--- spec/WAPJSONReader.spec.ts
+++ spec/WAPJSONReader.spec.ts
@@ -1,6 +1,25 @@
 
-// import WAPJSONReader from '../src/WAPJSONReader;'
+import {
+    WAPTestTools,
+    WAPReader,
+    WebAssetPack,
+    PluginManager
+} from '@wap/test-tools';
+import WAPJSONReader from '../src/WAPJSONReader';
+import def from './support/def.json';
+import expectation from './support/test-pkg/data.json';
 
-// describe('WAPJSONReader', () => {
+describe('WAPJSONReader', () => {
+    let reader: WAPReader = new WAPReader();
 
-// });
+    beforeAll(() => {
+        PluginManager.getInstance().addPlugin('json', new WAPJSONReader());
+    });
+
+    it('reads file properly', async () => {
+        let buffer: ArrayBuffer = await WAPTestTools.compile(def);
+        let pack: WebAssetPack = await reader.read(buffer);
+        let data: any = await pack.get('test.json');
+        expect(data).toEqual(expectation);
+    });
+});
